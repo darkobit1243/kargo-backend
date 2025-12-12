ce19e09ba894ed9e6100972adfb2860bfb1738a4
efeta <efeta@EFE-LAPTOP>
2025-12-12 20:55:39 +0300

    Son geliştirmeler veya hata düzeltmeleri3

--- src/deliveries/deliveries.module.ts
+++ src/deliveries/deliveries.module.ts
@@ -4,9 +4,10 @@ import { DeliveriesService } from './deliveries.service';
 import { DeliveriesController } from './deliveries.controller';
 import { WsModule } from '../ws/ws.module';
 import { Delivery } from './delivery.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [WsModule, TypeOrmModule.forFeature([Delivery])],
+  imports: [WsModule, TypeOrmModule.forFeature([Delivery]), AuthModule],
   controllers: [DeliveriesController],
   providers: [DeliveriesService],
 })
