86cdafeb849427a4e077eca9223590804553165e
efeta <efeta@EFE-LAPTOP>
2025-12-22 22:40:59 +0300

    CanlıKonum22

--- src/push/push.module.ts
+++ src/push/push.module.ts
@@ -1,11 +1,18 @@
+
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { User } from '../auth/user.entity';
 import { PushController } from './push.controller';
 import { PushService } from './push.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    ConfigModule,
+    JwtModule.register({}),
+  ],
   controllers: [PushController],
   providers: [PushService],
   exports: [PushService],
